670bf74d5e1cf15b2608d36ebc2b4b390afafd75
Mark Kelly <mkelly28@tcd.ie>
2014-05-19 18:41:38 +0100

    ssl example

--- src/test/java/BetamaxTest.java
+++ src/test/java/BetamaxTest.java
@@ -1,6 +1,7 @@
 import co.freeside.betamax.Betamax;
 import co.freeside.betamax.Recorder;
 import co.freeside.betamax.TapeMode;
+import co.freeside.betamax.httpclient.BetamaxHttpsSupport;
 import co.freeside.betamax.httpclient.BetamaxRoutePlanner;
 import co.freeside.betamax.proxy.jetty.ProxyServer;
 import com.sun.jersey.api.client.ClientResponse;
@@ -9,12 +10,15 @@ import jersey.DefaultRestResourceSupplier;
 import jersey.RestResourceSupplier;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.SystemDefaultHttpClient;
 import org.junit.Rule;
 import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -25,17 +29,37 @@ import static org.junit.Assert.assertThat;
 public class BetamaxTest {
 
     public static final String EXCHANGE_RATE_API_URL = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml";
+    public static final String CARTRAWLER = "https://ota.cartrawler.com/cartrawlerota";
 
     @Rule
-    public Recorder recorder = new Recorder();
+    public Recorder recorder = buildBetaMaxRecorder();
 
     private ProxyServer proxyServer = new ProxyServer(recorder);
+
+    private Recorder buildBetaMaxRecorder() {
+        Recorder recorder = new Recorder();
+        recorder.setSslSupport(true);
+        return recorder;
+    }
 
     @Betamax(tape = "euro-exchange-response-recording",
              mode = TapeMode.WRITE_ONLY)
     @Test
     public void shouldRecordData() throws IOException {
-        HttpResponse response = getHttpResponseUsingApacheDefaultClient();
+        HttpResponse response = getHttpResponseUsingApacheDefaultClient(EXCHANGE_RATE_API_URL);
+        assertThat(response.getStatusLine().getStatusCode(), is(200));
+    }
+
+    @Betamax(tape = "cartrawler-response-recording",
+            mode = TapeMode.WRITE_ONLY)
+    @Test
+    public void shouldRecordDataForCarTrawler() throws IOException, URISyntaxException {
+        DefaultHttpClient client = new SystemDefaultHttpClient();
+        HttpPost post = new HttpPost();
+        post.setEntity(new StringEntity("", "UTF-8"));
+        post.setURI(new URI(CARTRAWLER));
+        BetamaxHttpsSupport.configure(client);
+        HttpResponse response = client.execute(post);
         assertThat(response.getStatusLine().getStatusCode(), is(200));
     }
 
@@ -43,14 +67,14 @@ public class BetamaxTest {
     @Betamax(tape = "euro-exchange-response",
             mode = TapeMode.READ_ONLY)
     public void shouldReplayExchangeRateResponseWithDefaultHttpClient() throws Exception {
-        HttpResponse response = getHttpResponseUsingApacheDefaultClient();
+        HttpResponse response = getHttpResponseUsingApacheDefaultClient(EXCHANGE_RATE_API_URL);
         assertThat(response.getStatusLine().getStatusCode(), is(201));
     }
 
-    private HttpResponse getHttpResponseUsingApacheDefaultClient() throws IOException {
+    private HttpResponse getHttpResponseUsingApacheDefaultClient(String url) throws IOException {
         DefaultHttpClient client = new DefaultHttpClient();
         BetamaxRoutePlanner.configure(client);
-        HttpGet getMethod = new HttpGet(EXCHANGE_RATE_API_URL);
+        HttpGet getMethod = new HttpGet(url);
         return client.execute(getMethod);
     }
 
